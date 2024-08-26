f05f3db4613d4c1e02ee411afcf8dd8c89a8156e
Xaidel <abechuelak@gmail.com>
2024-08-26 18:19:14 +0800

    fix

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
-	"io"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-type Fact struct{
+type Fact struct {
 	Fact string `json:"fact"`
 }
 
-func main(){
+func main() {
 	res, err := http.Get("https://catfact.ninja/fact")
-	defer res.Body.Close()
-	
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+	defer res.Body.Close()
+
 	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	var fact Fact
 	err = json.Unmarshal(body, &fact)
-
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 
 	fmt.Println(fact.Fact)
 }
